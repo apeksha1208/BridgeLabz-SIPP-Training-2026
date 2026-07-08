f641f42d4513f0e3cd6515faaf9f7c3142222489
apeksha singh <apekshasingh2254@gmail.com>
2026-07-08 12:53:02 +0530

    [Apeksha]:added binary_search2d

---
+++ src/CollectionsAndDSA/gcr_codebase/Binary_Search/binary_search2d.java
@@ -0,0 +1,29 @@
+package Binary_Search;
+
+public class binary_search2d {
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int low = 0;
+        int high = rows * cols - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int row = mid / cols;
+            int col = mid % cols;
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60}
+        };
+        System.out.println(searchMatrix(matrix, 16));
+    }
+}
